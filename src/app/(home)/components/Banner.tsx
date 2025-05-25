46a9beafbbb5a60039bfe55ad6980578636ffe83
root4nishant <srivastava4nishant@gmail.com>
2025-05-25 17:44:09 +0530

    Refactor: Changed banner color scheme.

--- src/app/(home)/components/Banner.tsx
+++ src/app/(home)/components/Banner.tsx
@@ -21,11 +21,11 @@ const Banner: React.FC<BannerProps> = ({
         <div className="container mx-auto px-4  text-center flex flex-col lg:gap-8 md:gap-6 gap-3">
           <h2 className="text-sm lg:text-[36px] md:text-[24px] font-bold text-white">
             {message}
-            <span className="text-[#D6CFB6] leading-none">{message2}</span>
+            <span className="text-gray-300 leading-none">{message2}</span>
           </h2>
           <div className="flex justify-center gap-6">
             <a target="_blank" rel="noopener noreferrer " href={buttonLink}>
-              <Button className="lg:p-6 p-4 lg:w-[172px] md:w-[120px] w-[96px] h-[38px] lg:h-[60px] md:h-[44px] bg-[#D6CFB6] text-primary-dark rounded-sm lg:text-lg font-bold text-xs shadow-md hover:bg-black hover:text-white transition-colors duration-300">
+              <Button className="lg:p-6 p-4 lg:w-[172px] md:w-[120px] w-[96px] h-[38px] lg:h-[60px] md:h-[44px] bg-gray-300 text-primary-dark rounded-sm lg:text-lg font-bold text-xs shadow-md hover:bg-black hover:text-white transition-colors duration-300">
                 {buttonText}
               </Button>
             </a>
