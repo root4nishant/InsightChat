c0c22f7b0b8b9716e2a860ce6c50ed6fea968e6f
root4nishant <srivastava4nishant@gmail.com>
2025-05-31 02:08:31 +0530

    Refactor: Remove unused Navbar import.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,7 @@
-import Navbar from "@/components/custom/nav";
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className=" mx-auto ">
-      {children}
-    </div>
-  );
+  return <div className=" mx-auto ">{children}</div>;
 }
